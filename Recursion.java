e5097579a6dcc6c918cb094d38b0cd071da926a6
obichouna <obichouna@stuy.edu>
2017-02-07 20:05:32 -0500

    Create Recursion.java
    I had to do this directly in the Github website due to my entire hard drive not working. I did not get to test.

---
+++ Recursion.java
@@ -0,0 +1,26 @@
+public class Recursion{
+	
+	public static String name(){
+		return "Bichouna,Othman";
+	}
+	
+	public static double sqrt(double n){
+		if (n < 0){
+			throw new IllegalArgumentException();
+		}else{
+			return helper(n, 1);
+	}
+	
+	
+	public static double helper(double n, double guess){
+		double betterGuess = (n / guess + guess) / 2;
+		if(closeEnough(betterGuess * betterGuess, n){
+			return betterGuess;
+		}
+    	return helper(n, betterGuess);
+		}
+  }
+	public static boolean closeEnough(double a, double b){
+		return (Math.abs(a - b) < .00000000001);
+	}
+}
