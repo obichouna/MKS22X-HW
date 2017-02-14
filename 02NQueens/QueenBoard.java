21874eafe573ff6d7686eec25bdf73b2ab17e43b
Othman Bichouna <obichouna@stuy.edu>
2017-02-13 22:54:56 -0500

    Finished a large part, still need to finish some other parts

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -4,16 +4,17 @@ public class QueenBoard{
     public int boardSize;
 
     public QueenBoard(int size){
-	board = new int[size][size];
-	boardSize = size;
-	for (int row = 0, row < size, row++){
-	    for (int col = 0, col < size, col++){
-		board[row][col] = 0;
-	    }
-	}
-    }
-    public boolean solve(){
-	return solveH(0);
+	     board = new int[size][size];
+	     boardSize = size;
+       solutionCount == -1;
+	       for (int row = 0; row < size; row++){
+	          for (int col = 0; col < size; col++){
+		            board[row][col] = 0;
+	             }
+	            }
+            }
+    public void solve(){
+	     solveH(0);
     }
 
     public boolean solveH(int col){
@@ -29,9 +30,65 @@ public class QueenBoard{
 	//    return true;
 	//}
 
-	if (col == boardSize){
-	    return true;
-	}
-	
+      if (col >= boardSize){
+        return true;
+      }
+      for (int row = 0; row < boardSize; row++){
+        if (addQueen(row, col)){
+          addQueen(row, col);
+          if (solveH(col + 1)){
+            return true;
+          }else{
+            removeQueen(row, col);
+          }
+        }
+      }
+      return false;
+  }
+
+
+
+    private boolean addQueen(int row, int col){
+      if (board[row][col] != 0){
+        return false;
+      }
+      for (int i = 0; i < boardSize; i++){
+        board[row][i] += 1;
+        board[i][col] += 1;
+        if (row + i < boardSize && col + i < boardSize){
+          board[row + i][col + i] += 1;
+        }
+        if (row + i < boardSize && col - i >= 0){
+          board[row + i][col - i] += 1;
+        }
+        if (row - i >= 0 && col + i < boardSize){
+          board[row - i][col + i] += 1;
+        }
+        if (row - i >= 0 && col - i >= boardSize){
+          board[row - i][col - i] += 1;
+        }
+      }
+      board[row][col] = -1;
+    }
+
+    private boolean removeQueen(int row, int col){
+      for (int i = 0; i < boardSize; i++){
+        board[row][i] += -1;
+        board[i][col] += -1;
+        if (row + i < boardSize && col + i < boardSize){
+          board[row + i][col + i] += -1;
+        }
+        if (row + i < boardSize && col - i >= 0){
+          board[row + i][col - i] += -1;
+        }
+        if (row - i >= 0 && col + i < boardSize){
+          board[row - i][col + i] += -1;
+        }
+        if (row - i >= 0 && col - i >= boardSize){
+          board[row - i][col - i] += -1;
+        }
+      }
+      board[row][col] = 0;
+    }
     }
 }
