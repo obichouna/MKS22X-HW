7225e379c5572b8b23165f89d6b298a50402714c
Othman Bichouna <obichouna@stuy.edu>
2017-02-25 22:31:49 -0500

    worked a bit

--- 03WhatIs/KnightBoard.java
+++
@@ -1,6 +0,0 @@
-public class KnightBoard{
-    public int[][] board;
-    public 
-
-
-}
--- 04NeverGonna/KnightBoard.java
+++ 04NeverGonna/KnightBoard.java
@@ -1,21 +1,30 @@
 public KnightBoard{
-    public int[][]board;
-    public int x, y;
-    public int rows, cols;
+  public int[][] board;
+  public int x, y;
+  public int rows, cols;
 
-    public KnightBoard(int startingRows, int startingCols){
-	rows = startingRows;
-	cols = startingCols;
-	board = new int[startingRows][startingCols];
-	for (int row = 0; row < startingRows; row++){
-	    for (int col = 0; col < startingCols; col++){
-		board[row][col] = 0;
-	    }
-	}
+  public KnightBoard(int startingRows, int startingCols){
+    rows = startingRows;
+    cols = startingCols;
+    board = new int[startingRows][startingCols];
+    for (int row = 0; row < startingRows; row++){
+      for (int col = 0; col < startingCols; col++){
+        board[row][col] = 0;
+      }
     }
-    }
+  }
 
-    
+  public void solve(){
+    solveH(0, 0, 1);
+  }
+
+  public boolean solveH(int row, int col, int level){
+    if (board[row][col] != 0){
+      return false;
+    }
+    board[row][col] = level;
+    if ()
+  }
 
 
 }
