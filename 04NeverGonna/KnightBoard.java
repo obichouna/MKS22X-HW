a689e9599076bdb5496f13316e366109d5e340e8
Othman Bichouna <obichouna@stuy.edu>
2017-02-27 21:57:55 -0500

    Finished. Tested different combinations before 8,8. 8,8 takes a loooooooong time.

--- 04NeverGonna/KnightBoard.java
+++ 04NeverGonna/KnightBoard.java
@@ -1,6 +1,5 @@
-public KnightBoard{
+public class KnightBoard{
   public int[][] board;
-  public int x, y = 0;
   public int rows, cols;
 
   public KnightBoard(int startingRows, int startingCols){
@@ -18,11 +17,43 @@ public KnightBoard{
     solveH(0, 0, 1);
   }
 
-  public boolean solveH(int row, int col, int level){
-      if (board[row][col] == 0){
-	  board[row][col] = level;
-	  
+  private boolean solveH(int row, int col, int level){
+    // checking if the knight completed the board
+    if (rows * cols < level){
+      return true;
+    }
+    // checking if the move goes off the board
+    if (row < 0 || col < 0 || row >= rows || col >= cols){
+      return false;
+    }
+    if (board[row][col] == 0){
+	    board[row][col] = level;
+      //set the level first, next check if there is any solution
+      if (solveH(row + 2, col + 1, level + 1) || solveH(row + 2, col - 1, level + 1) ||
+          solveH(row + 1, col - 2, level + 1) || solveH(row - 1, col - 2, level + 1) ||
+          solveH(row - 2, col - 1, level + 1) || solveH(row - 2, col + 1, level + 1) ||
+          solveH(row + 1, col + 2, level + 1) || solveH(row - 1, col + 2, level + 1)){
+            return true;
+      }
+      // no solution that move? Turn the spot back to 0
+      board[row][col] = 0;
+    }
+    return false;
   }
 
-
+  public String toString(){
+    String display = "";
+    for (int r = 0; r < rows; r++){
+      for (int c = 0; c < cols; c++){
+        display += board[r][c] + " ";
+      }
+      display += "\n";
+    }
+    return display;
+  }
+//  public static void main (String[]args) {
+//    KnightBoard x = new KnightBoard(8, 8);
+//    x.solve();
+//    System.out.println(x);
+//  }
 }
