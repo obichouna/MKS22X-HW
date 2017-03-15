8506a9f9ffee6e0f14b74ed385e71cd0525aeecf
Othman Bichouna <obichouna@stuy.edu>
2017-03-15 14:45:13 -0400

    Worked on it

---
+++ 07DesertYou/Quick.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+public class Quick{
+    public Quick(){
+    }
+
+    public static part(int[]data, int start, int end){
+	Random rand = new Random();
+	int pivot = rand.nextInt(start, end);
+	temp = data[data.length - 1];
+	data[data.length - 1] = data[pivot];
+	data[pivot] = temp;
+	for (int i = start; i < end; i--){
+	    if(data[data.length - 1] < data[i]){
+		for (
+	    }
+	}
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
