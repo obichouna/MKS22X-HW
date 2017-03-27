c16a69c484c9d26f2fcc49edc5c69cd31d1fa1d5
Othman Bichouna <obichouna@stuy.edu>
2017-03-27 00:07:29 -0400

    beep boop i am a robot

--- 07DesertYou/Quick.java
+++ 07DesertYou/Quick.java
@@ -3,7 +3,7 @@ public class Quick{
     public Quick(){
     }
 
-    public static int part(int[]data, int start, int end){
+    public static int[] part(int[]data, int start, int end){
 	Random rand = new Random();
 	int pivotI = rand.nextInt(end - start) + start;
 	int pivot = data[pivotI];
@@ -29,17 +29,24 @@ public class Quick{
 		data[i] = temp;
 		gt--;
 	    }
-
 	}
-
-
-
-
-
+  int[] x = {lt, gt};
+  return x;
     }
 
 
+    public static int[] quickSort(int[] data){
+      return quickSortH(data, 0, data.length - 1);
+    }
 
+    public static int[] quickSortH(int[] data, int left, int right){
+      if (left > right){
+        int[] x = part(data, left, right);
+        quickSortH(data, x[1]  + 1, right);
+        quickSortH(data, left, x[0] - 1);
+      }
+      return data;
+    }
 
 
 
