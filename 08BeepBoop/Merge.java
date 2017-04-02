679907ed7f278d16cd143046cf7cf81e4aca86a7
Othman Bichouna <obichouna@stuy.edu>
2017-04-02 15:52:43 -0400

    cant figure out why merge isnt working

--- 08BeepBoop/Merge.java
+++ 08BeepBoop/Merge.java
@@ -18,53 +18,54 @@ public class Merge{
     while(temp < right.length){
       right[temp] = data[left.length + temp];
     }
-
-    if (left.length > 1){
-      mergesort(left);
-    }
-    if(right.length > 1){
-      mergesort(right);
-    }
-
+    mergesort(left);
+    mergesort(right);
     merge(data, left, right);
   }
 
 
   public static void merge(int[] data, int[] part1, int[] part2){
-    if (part1.length + part2.length == data.length){
-      return;
-    }
     int i = 0;
     int j = 0;
     int k = 0;
     while (i < part1.length && j < part2.length){
-     if (part1[i] < part2[j])
-     {
+     if (part1[i] < part2[j]){
          data[k] = part1[i];
          i++;
-     }
-     else
-     {
+         k++;
+     }else{
          data[k] = part2[j];
          j++;
+         k++;
      }
-     k++;
  }
 
- while (i < part1.length)
- {
+ while (i < part1.length){
      data[k] = part1[i];
      i++;
      k++;
  }
 
- while (j < part2.length)
- {
+ while (j < part2.length){
      data[k] = part2[j];
      j++;
      k++;
  }
+}
 
+public static String toString(int[] x){
+  String beep = "[";
+  for (int i = 0; i < x.length; i++){
+    beep += Integer.toString(x[i]) + ", ";
+  }
+  beep += "]";
+  return beep;
+}
+
+public static void main(String[] args){
+  int[] data = {2, 5, 3, 23, 11, 37, 25, 9, 85, 20, 74, 19, 38, 58, 67, 38};
+  mergesort(data);
+  System.out.println(toString(data));
 }
 
 }
