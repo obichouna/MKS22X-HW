0a8fe94e3b4d56695dde1241aef3f4c6f4210cea
Othman Bichouna <obichouna@stuy.edu>
2017-04-25 00:00:35 -0400

    Bugs, need to fix

---
+++ 11StackEvaluation/StackCalc.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class StackCalc{
+
+  public static Double eval(String s){
+    Stack x = new Stack();
+    for (int i = 0; i < s.length(); i++){
+      if (isOp(s.charAt(i))){
+	  String temp1 = x.pop().toString();
+	  String temp2 = x.pop().toString();
+        x.push(apply(s.substring(i, i + 1), temp1, temp2));
+      }else{
+	  x.push(s.substring(i, i + 1));
+      }
+    }
+    String fin = x.peek().toString();
+    return Double.parseDouble(fin);
+  }
+
+  private static boolean isOp(char s){
+    if (s == '*' || s == '-' || s == '/' || s == '+'){
+      return true;
+    }
+    return false;
+  }
+
+  private static String apply(String op, String a, String b){
+    if(op.equals("*")){
+        return (Integer.parseInt(a) * Integer.parseInt(b)) + "";
+    }else if(op.equals("+")){
+        return (Integer.parseInt(a) + Integer.parseInt(b)) + "";
+    }else if(op.equals("/")){
+        return (Integer.parseInt(a) / Integer.parseInt(b)) + "";
+    }else if(op.equals("-")){
+        return (Integer.parseInt(a) - Integer.parseInt(b)) + "";
+    }
+    return " ";
+  }
+
+}
