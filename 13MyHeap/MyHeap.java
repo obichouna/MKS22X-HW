46f1087bfb877db814f824f61f3643119062af09
Othman Bichouna <obichouna@stuy.edu>
2017-05-08 14:47:00 -0400

    I understand My Heap now

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,5 +1,27 @@
-public class MyHeap{
-    private int constant = 1;
-    private 
+import java.util.*;
 
+public class MyHeap{
+    public String[] heap;
+    private int constant = 1;
+    public int size;
+    public boolean style;
+
+
+    public MyHeap(){
+	heap = new String[0];
+	size = 0;
+	style = true;
+    }
+    
+    public MyHeap(boolean type){
+	heap = new String[0];
+	style = type;
+	size = 0;
+    }
+
+    private void pushUp(int index){
+	if (style){
+	    
+	}
+    }
 }
