17d7d8bc3c5e593ebc949f4c15d19059073e74a2
Othman Bichouna <obichouna@stuy.edu>
2017-05-17 14:44:20 -0400

    Priority next

---
+++ 15MazeIsBack/PriorityQueue.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class PriorityQueue implements Frontier{
+    public 
+
+
+
+
+
+}
--- 15MazeIsBack/StackFrontier.java
+++ 15MazeIsBack/StackFrontier.java
@@ -1,10 +1,23 @@
 import java.util.*;
 
 public class StackFrontier implements Frontier{
-    public Stack Locations;
+    public Stack<Location> Locations;
 
-    public StackFrontier(
+    public StackFrontier(){
+        Locations = new Stack<Location>();
+    }
 
 
+    public void add(Location x){
+	Locations.push(x);
+    }
+
+    public Location next(){
+	return Locations.peek();
+    }
+
+    public int size(){
+	return Locations.size();
+    }
 
 }
